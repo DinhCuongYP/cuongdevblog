51dfb98005ab3d46a4709c7a5e2a8563cd8b42da
DinhCuong <nguyendinhcuongyp@gmail.com>
2023-04-21 10:00:26 +0700

    Install Prettier Husky Lint-staged

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ const port = 3000;
 
 const route = require('./routes');
 
-        app.use(express.urlencoded({ extended: true }));
-        app.use(express.json());
-        app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(morgan("combined"));
 // Template Engine
-app.engine('hbs', handlebars({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+    app.engine('hbs', handlebars({ extname: '.hbs' }));
+    app.set('view engine', 'hbs');
+    app.set('views', path.join(__dirname, 'resources/views'));
 
 // Router Init
 route(app);
